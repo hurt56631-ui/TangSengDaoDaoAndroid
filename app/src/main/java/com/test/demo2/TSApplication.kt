cb52082b7ad5f5f43175148e6f78ac8d048a2b5a
hurt56631-ui <hurt56631@gmail.com>
2026-04-17 20:32:25 +0630

    Update TSApplication.kt

--- app/src/main/java/com/test/demo2/TSApplication.kt
+++ app/src/main/java/com/test/demo2/TSApplication.kt
@@ -109,7 +109,7 @@ class TSApplication : MultiDexApplication() {
     private fun initApi() {
         var apiURL = WKSharedPreferencesUtil.getInstance().getSP("api_base_url")
         if (TextUtils.isEmpty(apiURL)) {
-            apiURL = "https://api.botgate.cn"
+            apiURL = "http://107.172.79.50:8090"
             WKApiConfig.initBaseURL(apiURL)
         } else {
             WKApiConfig.initBaseURLIncludeIP(apiURL)
